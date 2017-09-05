62fe046f2810c128a3b26f8f0ffcc6299e459616
henylee <asqaws1@gmail.com>
2017-09-05 16:26:18 +0900

    홈화면(뉴스피드) / 리스트뷰 작성

---
+++ app/src/main/java/com/example/the/instacopy/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.example.the.instacopy;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by the on 2017-09-05.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+
+}
---
+++ app/src/main/java/com/example/the/instacopy/data/HomeData.java
@@ -0,0 +1,8 @@
+package com.example.the.instacopy.data;
+
+/**
+ * Created by the on 2017-09-05.
+ */
+
+public class HomeData {
+}

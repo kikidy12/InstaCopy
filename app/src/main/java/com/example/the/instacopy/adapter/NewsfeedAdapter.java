24c9014da5067f094fe7d4fd26a5e4ec91a70127
kikidy12 <kikidy12@naver.com>
2017-09-06 23:07:52 +0900

    디자인 일부 수정 및 data구성

--- app/src/main/java/com/example/the/instacopy/adapter/NewsfeedAdapter.java
+++ app/src/main/java/com/example/the/instacopy/adapter/NewsfeedAdapter.java
@@ -1,12 +1,15 @@
 package com.example.the.instacopy.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import com.example.the.instacopy.R;
 import com.example.the.instacopy.data.NewsfeedData;
@@ -22,6 +25,7 @@ public class NewsfeedAdapter extends ArrayAdapter<NewsfeedData> {
     Context mContext;
     List<NewsfeedData> mList;
     LayoutInflater inf;
+    String[] seemore = {"보관","공유하기","수정","삭제","댓글기능해제","공유URL복사"};
 
     public NewsfeedAdapter(Context context, List<NewsfeedData> list) {
         super(context, R.layout.newsfeed_list_item, list);
@@ -39,6 +43,21 @@ public class NewsfeedAdapter extends ArrayAdapter<NewsfeedData> {
         if (row==null) {
             row = inf.inflate(R.layout.newsfeed_list_item, null);
         }
+
+        ImageView seeMoreBtn = (ImageView) row.findViewById(R.id.seeMoreBtn);
+        seeMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setItems(seemore, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+            }
+        });
+
         return row;
     }
 
---
+++ app/src/main/java/com/example/the/instacopy/data/ReplyData.java
@@ -0,0 +1,57 @@
+package com.example.the.instacopy.data;
+
+import java.io.Serializable;
+
+/**
+ * Created by one on 2017-09-06.
+ */
+
+public class ReplyData implements Serializable {
+    private int replyId; // 댓글 아이디
+    private int newsfeedId; // 댓글이 달리는 뉴스피드 아이디
+    private String replyContent; // 댓글 내용
+
+    private User writer; // 댓글을 단 사용자의 정보
+
+    public ReplyData() {
+    }
+
+    public ReplyData(int replyId, int newsfeedId, String replyContent, User writer) {
+        this.replyId = replyId;
+        this.newsfeedId = newsfeedId;
+        this.replyContent = replyContent;
+        this.writer = writer;
+    }
+
+    public int getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(int replyId) {
+        this.replyId = replyId;
+    }
+
+    public int getNewsfeedId() {
+        return newsfeedId;
+    }
+
+    public void setNewsfeedId(int newsfeedId) {
+        this.newsfeedId = newsfeedId;
+    }
+
+    public String getReplyContent() {
+        return replyContent;
+    }
+
+    public void setReplyContent(String replyContent) {
+        this.replyContent = replyContent;
+    }
+
+    public User getWritter() {
+        return writer;
+    }
+
+    public void setWritter(User writter) {
+        this.writer = writter;
+    }
+}

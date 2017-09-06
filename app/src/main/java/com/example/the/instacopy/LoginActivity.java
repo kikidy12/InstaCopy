c5ef0e34f3970ade191091ae125073053d9e2c72
ChoiJongHwan <cjh19890212@gmail.com>
2017-09-06 13:11:58 +0900

    Login XML 구현중 // 키해쉬등록까지완료

--- app/src/main/java/com/example/the/instacopy/LoginActivity.java
+++ app/src/main/java/com/example/the/instacopy/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.example.the.instacopy;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends BaseActivity {
 
@@ -13,6 +21,23 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.the.instacopy",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+
         bindViews();
         setupEvents();
         setValues();

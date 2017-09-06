8e28aaecd5a28698ce7c0be4a4edaaf2222ede6f
ChoiJongHwan <cjh19890212@gmail.com>
2017-09-06 15:59:32 +0900

    스플래쉬 액티비티 구현 완료

--- app/src/main/java/com/example/the/instacopy/SplashActivity.java
+++ app/src/main/java/com/example/the/instacopy/SplashActivity.java
@@ -1,7 +1,19 @@
 package com.example.the.instacopy;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.example.the.instacopy.utils.GlobalData;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashActivity extends BaseActivity {
 
@@ -10,23 +22,34 @@ public class SplashActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        bindViews();
-        setupEvents();
-        setValues();
-    }
 
-    @Override
-    public void bindViews() {
 
     }
 
     @Override
     public void setupEvents() {
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent myIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(myIntent);
+                finish();
+
+            }
+        }, 2000);
+
+        GlobalData.initGlobalData();
+
     }
 
     @Override
     public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
 
     }
 }

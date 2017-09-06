e8a825b8a23c34677f8670c345e9868fdfc0becd
ChoiJongHwan <cjh19890212@gmail.com>
2017-09-06 13:02:46 +0900

    Login XML 구현중 // 로그인액티비티->메인화면넘기는 이벤트처리완료 //

--- app/src/main/java/com/example/the/instacopy/LoginActivity.java
+++ app/src/main/java/com/example/the/instacopy/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.example.the.instacopy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends BaseActivity {
+
+    private android.widget.Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         bindViews();
         setupEvents();
         setValues();
@@ -17,6 +20,21 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                UserData myData = new UserData(542, "아이유", "iu", "아이유사진경로");
+//
+//                // ContextUtil을 이용해서 저장.
+//                ContextUtil.setLoginUser(mContext, myData);
+
+                Intent intent = new Intent(mContext, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
 
     }
 
@@ -27,6 +45,8 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
 
     }
 }
+

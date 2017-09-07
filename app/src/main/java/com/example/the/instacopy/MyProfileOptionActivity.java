ef12b8000c7d511b4bdc61176a4d5bd54cd010ae
ChoiJongHwan <cjh19890212@gmail.com>
2017-09-07 12:14:03 +0900

    사용자-더보기의 로그아웃 시 경고창띄우고 로그인액티비티까지구현완료

--- app/src/main/java/com/example/the/instacopy/MyProfileOptionActivity.java
+++ app/src/main/java/com/example/the/instacopy/MyProfileOptionActivity.java
@@ -1,5 +1,7 @@
 package com.example.the.instacopy;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -27,11 +29,26 @@ public class MyProfileOptionActivity extends BaseActivity {
         LogoutTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(mContext, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MyProfileOptionActivity.this);
+                builder.setTitle ( " 로그아웃 " );
+                builder.setMessage ( " 정말 로그아웃하시겠습니까? " );
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(mContext, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton ( "취소", null );
+
+
+                builder.show();
             }
         });
+
+
+
 
         //                계정추가 버튼 누를 시 적용되는 이벤트
 

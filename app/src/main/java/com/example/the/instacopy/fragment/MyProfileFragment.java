669133908d2a99849b04349c2f4aa124788c2a2a
ChoiJongHwan <cjh19890212@gmail.com>
2017-09-07 12:06:39 +0900

    사용자프로필의 더보기버튼클릭이벤트처리완료

--- app/src/main/java/com/example/the/instacopy/fragment/MyProfileFragment.java
+++ app/src/main/java/com/example/the/instacopy/fragment/MyProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.the.instacopy.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.the.instacopy.MyProfileOptionActivity;
 import com.example.the.instacopy.R;
 import com.example.the.instacopy.adapter.NewsfeedAdapter;
 import com.example.the.instacopy.adapter.PhotoAdapter;
@@ -64,6 +66,15 @@ public class MyProfileFragment extends Fragment {
     }
 
     private void setupEvents() {
+
+        seeMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MyProfileOptionActivity.class);
+                startActivity(intent);
+            }
+        });
+
         photoView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

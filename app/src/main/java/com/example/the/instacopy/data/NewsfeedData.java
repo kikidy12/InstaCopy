e2a1cbb1b11eea14d91c15cc2557cb6551b1d119
henylee <asqaws1@gmail.com>
2017-10-11 16:07:45 +0900

    오류해결

--- app/src/main/java/com/example/the/instacopy/data/NewsfeedData.java
+++ app/src/main/java/com/example/the/instacopy/data/NewsfeedData.java
@@ -25,7 +25,7 @@ public class NewsfeedData implements Serializable {
 
         try {
             tempNews.setNewsfeedId(json.getInt("id"));
-            tempNews.setImageURL(json.getString("profile_url"));
+            tempNews.setImageURL(json.getString("image_resource"));
 
             tempNews.setWriter(User.getUserFromJsonObject(json.getJSONObject("user")));
         } catch (JSONException e) {
